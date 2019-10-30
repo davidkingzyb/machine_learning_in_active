de55bbc4256ab7c09d9b4e5860adcbaa4164cc27
DKZ <davidkingzyb@163.com>
2019-10-30 17:44:10 +0800

    recommend engine

--- Ch14/svdRec.py
+++ Ch14/svdRec.py
@@ -7,10 +7,17 @@ from numpy import *
 from numpy import linalg as la
 
 def loadExData():
-    return[[0, 0, 0, 2, 2],
-           [0, 0, 0, 3, 3],
-           [0, 0, 0, 1, 1],
-           [1, 1, 1, 0, 0],
+    # return[[0, 0, 0, 2, 2],
+    #        [0, 0, 0, 3, 3],
+    #        [0, 0, 0, 1, 1],
+    #        [1, 1, 1, 0, 0],
+    #        [2, 2, 2, 0, 0],
+    #        [5, 5, 5, 0, 0],
+    #        [1, 1, 1, 0, 0]]
+    return[[4,4, 0, 2, 2],
+           [4, 0, 0, 3, 3],
+           [4, 0, 0, 1, 1],
+           [1, 1, 1, 2, 0],
            [2, 2, 2, 0, 0],
            [5, 5, 5, 0, 0],
            [1, 1, 1, 0, 0]]
@@ -47,11 +54,12 @@ def standEst(dataMat, user, simMeas, item):
         userRating = dataMat[user,j]
         if userRating == 0: continue
         overLap = nonzero(logical_and(dataMat[:,item].A>0, \
-                                      dataMat[:,j].A>0))[0]
+                                      dataMat[:,j].A>0))[0]#[userindex]
+        print(overLap,userRating)
         if len(overLap) == 0: similarity = 0
         else: similarity = simMeas(dataMat[overLap,item], \
                                    dataMat[overLap,j])
-        print 'the %d and %d similarity is: %f' % (item, j, similarity)
+        print('the %d and %d similarity is: %f' % (item, j, similarity))
         simTotal += similarity
         ratSimTotal += similarity * userRating
     if simTotal == 0: return 0
@@ -68,16 +76,16 @@ def svdEst(dataMat, user, simMeas, item):
         if userRating == 0 or j==item: continue
         similarity = simMeas(xformedItems[item,:].T,\
                              xformedItems[j,:].T)
-        print 'the %d and %d similarity is: %f' % (item, j, similarity)
+        print('the %d and %d similarity is: %f' % (item, j, similarity))
         simTotal += similarity
         ratSimTotal += similarity * userRating
     if simTotal == 0: return 0
     else: return ratSimTotal/simTotal
 
 def recommend(dataMat, user, N=3, simMeas=cosSim, estMethod=standEst):
-    unratedItems = nonzero(dataMat[user,:].A==0)[1]#find unrated items 
+    unratedItems = nonzero(dataMat[user,:].A==0)[1]#find unrated items [itemindex]
     if len(unratedItems) == 0: return 'you rated everything'
-    itemScores = []
+    itemScores = []#[(itemindex,score)]
     for item in unratedItems:
         estimatedScore = estMethod(dataMat, user, simMeas, item)
         itemScores.append((item, estimatedScore))
@@ -87,9 +95,9 @@ def printMat(inMat, thresh=0.8):
     for i in range(32):
         for k in range(32):
             if float(inMat[i,k]) > thresh:
-                print 1,
-            else: print 0,
-        print ''
+                print(1),
+            else: print(0),
+        print('')
 
 def imgCompress(numSV=3, thresh=0.8):
     myl = []
@@ -99,12 +107,16 @@ def imgCompress(numSV=3, thresh=0.8):
             newRow.append(int(line[i]))
         myl.append(newRow)
     myMat = mat(myl)
-    print "****original matrix******"
+    print("****original matrix******")
     printMat(myMat, thresh)
     U,Sigma,VT = la.svd(myMat)
     SigRecon = mat(zeros((numSV, numSV)))
     for k in range(numSV):#construct diagonal matrix from vector
         SigRecon[k,k] = Sigma[k]
     reconMat = U[:,:numSV]*SigRecon*VT[:numSV,:]
-    print "****reconstructed matrix using %d singular values******" % numSV
+    print("****reconstructed matrix using %d singular values******" % numSV)
     printMat(reconMat, thresh)
+
+if __name__=='__main__':
+    result=recommend(mat(loadExData()),2)
+    print(result)
